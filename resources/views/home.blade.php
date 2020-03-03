86c32a85bda3b28927f6be9562021d614524f597
Jamie White <jamie.white@adyen.com>
2020-03-03 17:12:08 +0000

    Updates

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('content')
+  <div class="jumbotron see-through">
+    <div class="container mild-opacity">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-sm-8 offset-sm-2">
+          <h1 class="text-center">Example Adyen Integrations</h1>
+          <ul class="list-group">
+            <li class="list-group-item">Laravel</li>
+            <li class="list-group-item">Knockout.js</li>
+            <li class="list-group-item">CSS</li>
+            <li class="list-group-item">jQuery</li>
+            <li class="list-group-item">Adyen API</li>
+            <li class="list-group-item"></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
